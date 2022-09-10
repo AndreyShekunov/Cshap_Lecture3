c0cd10ddfdb268b402cd8cda91a832410ce29332
Шекунов Андрей <asheknov@gmail.com>
2022-09-10 20:22:59 +0500

    Example012_2 синтаксис цикла for

---
+++ Example012_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Цикл For (цкл со счётчиком). Но нужно отметить все циклы взаимозаменяемые (for, while)
+// 99% задач легко решаются с помощью цикла for
+
+// подправим 4 Метод из Example012_1
+
+string Method4(int count, string text)
+{
+    string result = String.Empty; // String.Empty означает "Пустая строка", можно заменить на " ".
+    for(int i = 0; i < count; i++) // for(инициализация счетчика(int i = 0); проверка условия(i < count); инкримент(i++))
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "z");
+Console.WriteLine(res);
